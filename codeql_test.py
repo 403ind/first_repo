56dd11531446dcd81f9c389ddfee0880b39ab69e
Alex Kinnane <17098249+akinnane@users.noreply.github.co>
2023-03-10 14:24:45 +0000

    Add bad code x2e

--- codeql_test.py
+++ codeql_test.py
@@ -3,7 +3,7 @@ import re
 
 app = Flask(__name__)
 
-UNSAFE_REGEX = re.compile("(www|beta)\.example.com/")
+UNSAFE_REGEX = re.compile("(www|beta).example.com/")
 SAFE_REGEX = re.compile(r"(www|beta)\.example\.com/")
 
 @app.route('/some/path/bad')
@@ -17,6 +17,12 @@ def safe(request):
     target = request.args.get('target', '')
     if SAFE_REGEX.match(target):
         return redirect(target)
+
+@app.route("/command1")
+def command_injection1():
+    files = request.args.get('files', '')
+    # Don't let files be `; rm -rf /`
+    os.system("ls " + files) # $result=BAD    
 
 # FP reported in https://github.com/github/codeql/issues/3712
 # This does not define a regex (but could be used by other code to do so)
