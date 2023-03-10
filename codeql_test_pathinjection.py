56b21d6dc2500a1d4d83a30de1e7b6eb973bd53d
Alex Kinnane <17098249+akinnane@users.noreply.github.co>
2023-03-10 14:26:03 +0000

    Add path injection

---
+++ codeql_test_pathinjection.py
@@ -0,0 +1,19 @@
+import os.path
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+def source():
+    return request.args.get("path", "")
+
+
+def normalize(x):
+    return os.path.normpath(x)
+
+
+@app.route("/path")
+def simple():
+    x = source()
+    open(x)  # $result=BAD
